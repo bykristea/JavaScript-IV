5578eb057166c1b1f97e851321e51d1d38778910
Kristea Gamel <krgamel@gmail.com>
2019-01-24 14:59:03 -0600

    Instructors and Student Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,38 @@ class Person {
     }
 }
 
+class Instructor extends Person {
+    constructor (attributes) {
+        super(attributes)
+
+        this.specialty = attributes.specialty
+        this.favLanguage = attributes.favLanguage
+        this.catchPhrase = attributes.catchPhrase
+    }
+
+    demo (subject) {
+        return `Today we are learning about {subject}.`
+    }
+
+    grade (name, subject) {
+        return ` ${student.name} receives a perfect score on {subject}`
+    }
+}
+
+class Student extends Instructor {
+    constructor (attributes) {
+        super(attributes)
+        this.previousBackground = attributes.previousBackground
+        this.className = attributes.className
+        this.favSubjects = attributes.favSubjects
+    }
+
+    PRAssignment (subject) {
+        return `${this.name} has submitted a PR for {subject}`;
+    }
+    sprintChallenge (subject) {
+        `${this.name} has begun sprint challenge on {subject}`;
+    }
+}
+
+
