97d30bc560731a4b20ecdd5f4fe98fa726c38a7f
Kristea Gamel <krgamel@gmail.com>
2019-01-24 21:36:57 -0600

    mvp

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,6 +27,10 @@ class Instructor extends Person {
     grade (student, subject) {
         return ` ${student.name} receives a perfect score on ${subject}`
     }
+
+    score (student, grade) {
+        return 
+    }
 }
 
 class Student extends Person {
@@ -37,11 +41,17 @@ class Student extends Person {
         this.favSubjects = attributes.favSubjects
     }
 
+
     PRAssignment (subject) {
-        return `${student.name} has submitted a PR for ${subject}`;
+        return `${this.name} has submitted a PR for ${subject}`;
     }
+
     sprintChallenge (subject) {
-        `${this.name} has begun sprint challenge on ${subject}`;
+      return  `${this.name} has begun sprint challenge on ${subject}`;
+    }
+
+    listsSubjects () {
+        this.favSubjects.forEach(console.log);
     }
 }
 
@@ -54,11 +64,11 @@ class ProjectManager extends Instructor {
     }
 
     standUp (channel) {
-        return `${this.name} announces to {channel}, @channel stand up time!`
+        return `${this.name} announces to ${channel}, @channel stand up time!`
     }
 
     debugsCode (student, subject) {
-        return `${this.name} debugs {student.name}'s code on ${subject}`
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
     }
 
 }
@@ -83,5 +93,63 @@ const tom = new Instructor({
     catchPhrase: 'How you doin',
 
 });
+
+
+const kristea = new Student({
+        name: 'Kristea',
+        location: 'Home',
+        age: 33,
+        gender: 'Female',
+        previousBackground: '9 to 5',
+        className: 'Webpt2',
+        favSubjects: ['HTML', 'CSS', 'JS'],
+        grade: 98
+});
+
+const bo = new Student({
+    name: 'Bo',
+    location: 'France',
+    age: 22,
+    gender: 'Male',
+    previousBackground: 'cashier',
+    className: 'The CS12 4',
+    favSubjects: ['JavaScript', 'CSS', 'JS'],
+    grade: 92
+});
+
+const barb = new ProjectManager({
+    name: 'Barb',
+    location: 'Ohio',
+    age: 45,
+    gender: 'Female',
+    specialty: 'front-end',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Dontchya know',
+    gradClassName: 'Web01',
+    favInstructor: 'Jared'
+
+});
+
+const steven = new ProjectManager({
+    name: 'Steven',
+    location: 'Alaska',
+    age: 52,
+    gender: 'Male',
+    specialty: 'front-end',
+    favLanguage: 'ALL',
+    catchPhrase: 'Now we are cookin',
+    gradClassName: 'Web03',
+    favInstructor: 'himself'
+
+});
 console.log(susan.demo('CSS'));
+console.log(susan.grade(bo));
 console.log(tom.speak());
+console.log(tom.grade(kristea, 'CSS'));
+console.log(kristea.PRAssignment('CSS'));
+
+
+console.log(steven.standUp('webpt4'));
+console.log(bo.sprintChallenge('HTML'));
+console.log(barb.debugsCode(kristea, 'HTML'));
+bo.listsSubjects();
